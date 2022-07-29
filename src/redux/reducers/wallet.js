24735c763c90d3257f091652806bdc3798dd9f31
AlanoBurity <alanosantosdev@gmail.com>
2022-07-29 16:52:49 -0300

    requisito 1

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,8 +1,1 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-const INITIAL_STATE = {
-  carteira: [],
-};
-
-const wallet = (state = INITIAL_STATE, action) => state;
-
-export default wallet;
+// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas;
